b621cea4bab3a3161ecd5f21b2ffbafd046cf666
Hoyoung Jung <honux77@gmail.com>
2021-01-20 16:17:05 +0900

    Refactor test

--- src/test/java/net/honux/chess/BoardTest.java
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -4,18 +4,26 @@ import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class BoardTest {
+    Board board;
+
+    @BeforeEach
+    void setup() {
+        board = new Board();
+        assertThat(board).isNotNull();
+        assertThat(board.getSize()).isEqualTo(0);
+    }
 
     @Test
     @DisplayName("보드가 생성되고 폰을 추가할 수 있어야 한다.")
     void create() {
-        Board board = new Board();
-        assertThat(board).isNotNull();
-        assertThat(board.getSize()).isEqualTo(0);
-        board.add(new Pawn());
-        assertThat(board.getSize()).isEqualTo(1);
-        assertThat(board.getPawn(0).getColor()).isEqualTo(Pawn.WHITE);
-        board.add(new Pawn(Pawn.BLACK));
-        assertThat(board.getSize()).isEqualTo(2);
-        assertThat(board.getPawn(1).getColor()).isEqualTo(Pawn.BLACK);
+        checkAddPawn(Pawn.WHITE, 1);
+        checkAddPawn(Pawn.BLACK, 2);
+    }
+
+    void checkAddPawn(String color, int size) {
+        board.add(new Pawn(color));
+        assertThat(board.getSize()).isEqualTo(size);
+        assertThat(board.getPawn(size - 1).getColor()).isEqualTo(color);
     }
 }
+
