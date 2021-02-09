8e931e5416410dbd9781e28a10e5570c609af896
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-09 20:12:06 +1300

    Refactor PawnTest to reduce text duplication
    요구사항에 따라 로컬 변수를 선언해서 문자에 대한 중복 코드를 제거했습니다

--- src/test/java/net/honux/chess/PawnTest.java
+++ src/test/java/net/honux/chess/PawnTest.java
@@ -1,6 +1,7 @@
 package net.honux.chess;
 
 import org.junit.jupiter.api.*;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
@@ -8,10 +9,9 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        String color = "white";
 
-        Pawn pawn2 = new Pawn("black");
-        assertThat(pawn2.getColor()).isEqualTo("black");
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }
